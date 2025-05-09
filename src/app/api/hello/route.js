7ed127bfa3f0f64e24426c650344e3756687d063
tharushaudana <tharushaudana519@gmail.com>
2025-05-09 08:42:39 +0530

    gemini api tested (success)

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,6 +1,14 @@
-// app/api/hello/route.js
+import { GoogleGenAI } from "@google/genai";
+
 export async function GET() {
-    return new Response(JSON.stringify({ message: "Hello from the backend!" }), {
+    const ai = new GoogleGenAI({ apiKey: "AIzaSyDppMQH-QxUFphJhug9AaKPTGYOSipx8lA" });
+
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: "Explain how AI works in a few words",
+    });
+
+    return new Response(JSON.stringify({ message: response.text }), {
       status: 200,
       headers: {
         'Content-Type': 'application/json',
